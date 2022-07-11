b35ed3c48a5a10c7474e8582f0953417acd47e4c
Kalininskiy Andrei <kresthaus@mail.ru>
2022-07-11 21:12:38 +0400

    Готова № 1

--- Home № 1/Program.cs
+++ Home № 1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+Console.Clear();
+
+int Prompt(String message){
+    Console.Write(message);
+    string a_String = Console.ReadLine();
+    return int.Parse(a_String); 
+}
+void PrintNaturalNumbers(int numberM, int numberN){
+    if (numberM > numberN) return;
+    if (numberN % 2==1){
+        numberN--;
+    }
+    PrintNaturalNumbers(numberM, numberN-2);
+    System.Console.Write($"{numberN}; ");
+}
+
+int numberM = Prompt("Введите число 1: ");
+int numberN = Prompt("Введите число 2: ");
+
+System.Console.WriteLine();
+
+System.Console.WriteLine($"M = {numberM}, N = {numberN} -> ");
+PrintNaturalNumbers(numberM, numberN);
+System.Console.WriteLine();
